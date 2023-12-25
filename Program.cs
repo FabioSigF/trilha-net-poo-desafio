71bd1a71d0438deecc9cb7dcc5ab3441e0d6daf4
Fábio <fabiosgcontato@gmail.com>
2023-12-25 11:56:14 -0300

    Resolvendo desafio de POO dio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("34991585707", "A", "123456", 32);
+Iphone iphone = new Iphone("34991585706", "16", "78910", 256);
+
+iphone.InstalarAplicativo("Youtube");
+nokia.InstalarAplicativo("Youtube");
